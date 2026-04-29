1cbe3b4c8dec71133214f5253b82bb2e18ec8f40
AndreFRAPPIER <andre.frappier@epitech.eu>
2026-04-30 00:35:10 +0200

    [CORE] feat: class implementation (.cpp files)

---
+++ src/core/RaytracerError.cpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** RaytracerError
+*/
+
+#include "core/RaytracerError.hpp"
+
+namespace RayTracer {
+
+RaytracerError::RaytracerError(const std::string &message)
+    : _message(message)
+{
+}
+
+const char *RaytracerError::what() const noexcept
+{
+    return _message.c_str();
+}
+
+}
