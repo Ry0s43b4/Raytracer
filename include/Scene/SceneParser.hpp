6831a38f460ce939aa7fe2996a8d651810a1611e
AndreFRAPPIER <andre.frappier@epitech.eu>
2026-05-05 00:36:17 +0200

    [SCENE] fix: parser rewrited (cf pull request desc)

--- include/Scene/SceneParser.hpp
+++ include/Scene/SceneParser.hpp
@@ -1,95 +1,40 @@
 /*
 ** EPITECH PROJECT, 2026
-** G-OOP-400-PAR-4-1-raytracer-22
+** raytracer
 ** File description:
 ** SceneParser
 */
 
 #pragma once
-#include <iostream>
+
 #include <libconfig.h++>
 #include <string>
-#include <vector>
 
-typedef struct Camera {
-    int width;
-    int height;
-    int px;
-    int py;
-    int pz;
-    int rx;
-    int ry;
-    int rz;
-    double fov;
-} Camera_t;
+#include "scene/SceneData.hpp"
 
-typedef struct Sphere
-{
-    int x;
-    int y;
-    int z;
-    int r;
-    int cr;
-    int cg;
-    int cb;
-} Sphere_t;
-
-typedef struct Plane
-{
-    std::string axis;
-    int position;
-    int cr;
-    int cg;
-    int cb;
-} Plane_t;
-
-typedef struct PointLight {
-    int x;
-    int y;
-    int z;
-} PointLight_t;
-
-typedef struct DirectionalLight {
-    int x;
-    int y;
-    int z;
-} DirectionalLight_t;
-
-typedef struct Light {
-    double ambient;
-    double diffuse;
-    std::vector<PointLight_t> points;
-    std::vector<DirectionalLight_t> directionals;
-} Light_t;
-
-typedef struct Parsing {
-    Camera_t c;
-    std::vector<Sphere_t> s;
-    std::vector<Plane_t> p;
-} Parsing_t;
+namespace RayTracer {
 
 class SceneParser {
-    public:
-        SceneParser(std::string filepath) : filepath(filepath) {};
-        SceneParser() = default;
-        ~SceneParser() = default;
+public:
+    explicit SceneParser(const std::string &filepath);
 
-        int ReadConfigFile();
-        
-        // THIS GETTER IS FOR CAMERA
-        Camera_t GetCamera();
+    SceneData parse();
 
-        // THIS GETTER IS FOR SPHERES
-        std::vector<Sphere_t> GetSphere();
+private:
+    std::string _filepath;
+    libconfig::Config _config;
 
-        // THIS GETTER IS FOR PLANES
-        std::vector<Plane_t> GetPlane();
+    void readFile();
 
-        // THIS GETTER IS FOR LIGHTS
-        Light_t GetLight();
-        std::vector<PointLight_t> GetPointLight();
-        std::vector<DirectionalLight_t> GetDirectionalLight(); // FONCTION A VOIR CAR PAS IMPLEMENTER DANS LE FICHIER DE CONFIG
-    private:
-        libconfig::Config cfg;
-        std::string filepath;
+    CameraData parseCamera() const;
+    std::vector<SphereData> parseSpheres() const;
+    std::vector<PlaneData> parsePlanes() const;
+    LightData parseLights() const;
+
+    std::vector<PointLightData> parsePointLights() const;
+    std::vector<DirectionalLightData> parseDirectionalLights() const;
+
+    ColorData parseColor(const libconfig::Setting &setting) const;
 };
+
+}
