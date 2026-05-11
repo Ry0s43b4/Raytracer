fcad0ef37d2ca9dd41491959008a8b84e668341c
noo-noo <noah.heurteaut@epitech.eu>
2026-05-11 18:18:15 +0200

    [PRIMITIVE] feat: add full implementation of intersect methode in cylinder class

--- src/Primitives/Cylinder.cpp
+++ src/Primitives/Cylinder.cpp
@@ -23,8 +23,25 @@ namespace RayTracer {
     Intersection Cylinder::intersect(const Ray &ray) const
     {
         Math::Vector3D x = ray.origin() - _center;
-        double a = ray.direction().dot(ray.direction()) - (ray.direction().dot(_cylinderAxis))^2;
+
+        double a = ray.direction().dot(ray.direction()) - (ray.direction().dot(_cylinderAxis)) * (ray.direction().dot(_cylinderAxis));
         double b = 2 * ray.direction().dot(x) - (ray.direction().dot(_cylinderAxis) * x.dot(_cylinderAxis));
-        double c = x.dot(x) - x.dot(_cylinderAxis)^2 - _radius * _radius;
+        double c = x.dot(x) - x.dot(_cylinderAxis) * x.dot(_cylinderAxis) - _radius * _radius;
+        double discriminant = b * b - 4 * a * c;
+        
+        if (discriminant < 0)
+            return Intersection();
+
+            double distance = (-b - std::sqrt(discriminant)) / (2.0 * a);
+        if (distance <= 0.001) {
+            distance = (-b + std::sqrt(discriminant)) / (2.0 * a);
+            if (distance <= 0.001)
+                return Intersection();
+        }
+
+        Math::Vector3D point  = ray.at(distance);
+        Math::Vector3D normal = (point - _center).normalized();
+
+        return Intersection(true, distance, point, normal, _color);
     }
 }
