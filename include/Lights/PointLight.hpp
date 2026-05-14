215ef7ed1d3925aee2514677c52bbd15b4ff7806
AndreFRAPPIER <andre.frappier@epitech.eu>
2026-05-14 20:02:36 +0200

    feat: Point Lights implementation

---
+++ include/Lights/PointLight.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** PointLight
+*/
+
+#pragma once
+
+#include "Lights/ILight.hpp"
+
+namespace RayTracer {
+
+class PointLight : public ILight {
+public:
+    PointLight(const Math::Vector3D &position, double intensity);
+
+    Color computeLight(
+        const Intersection &intersection,
+        const Math::Vector3D &viewDir,
+        const std::vector<std::unique_ptr<IPrimitive>> &primitives
+    ) const override;
+
+private:
+    Math::Vector3D _position;
+    double _intensity;
+};
+
+}
