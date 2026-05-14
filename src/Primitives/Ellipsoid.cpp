a033003e20c3d7c53961478319a82bfaef7e6a97
noo-noo <noah.heurteaut@epitech.eu>
2026-05-14 22:06:26 +0200

    [PRIMITIVE] feat: implement intersect methode for Ellipsoid

--- src/Primitives/Ellipsoid.cpp
+++ src/Primitives/Ellipsoid.cpp
@@ -11,17 +11,37 @@ namespace RayTracer {
 
     Ellipsoid::Ellipsoid(
         const Math::Vector3D &center,
-        const Math::Vector3D &axis,
+        const Math::Vector3D &ellipsoidAxis,
         double radius,
         double distance,
         const Color &color
     )
-    : _center(center), _ellipsoidAxis(axis.normalized()), _radius(radius), _distance(distance), _color(color)
+    : _center(center), _ellipsoidAxis(ellipsoidAxis.normalized()), _radius(radius), _distance(distance), _color(color)
     {
     }
     
     Intersection Ellipsoid::intersect(const Ray &ray) const
     {
+        Math::Vector3D x = ray.origin() - _center;
+
+        double a = 4 * _radius * _radius * (ray.direction().dot(_ellipsoidAxis) * ray.direction().dot(_ellipsoidAxis));
+        double b = 2 * (_radius * _radius * ray.direction().dot(x) - 2 * (ray.direction().dot(_ellipsoidAxis)) * _distance * (_radius * _radius + 2 * x.dot(_ellipsoidAxis) * _distance - _distance));
+        double c = 4 * _radius * _radius * x.dot(x) - ((_radius * _radius + 2 * x.dot(_ellipsoidAxis) * _distance - _distance) * (_radius * _radius + 2 * x.dot(_ellipsoidAxis) * _distance - _distance));
         
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0)
+            return Intersection();
+
+        double distance = (-b - std::sqrt(discriminant)) / (2.0 * a);
+        if (distance <= 0.001) {
+            distance = (-b + std::sqrt(discriminant)) / (2.0 * a);
+            if (distance <= 0.001)
+                return Intersection();
+        }
+        Math::Vector3D point  = ray.at(distance);
+        Math::Vector3D cmid = _center + _ellipsoidAxis * (_distance / 2);
+        Math::Vector3D R = point - cmid;
+        Math::Vector3D normal = (R - _ellipsoidAxis * (1 - (b * b) / (a * a)) * (R.dot(_ellipsoidAxis)));
+        return Intersection(true, distance, point, normal.normalized(), _color);
     }
 }
